143d84ff4844065aa41bb686bb66434df5fed978
patrickpaterlini <patrick.patrick@outlook.com>
2020-08-06 20:27:17 -0300

    Adicionado JS nas páginas

---
+++ public/scripts/addField.js
@@ -0,0 +1,21 @@
+// Procurar o botao
+document.querySelector("#add-time")
+// Quando clicar no botao
+.addEventListener('click', cloneField)
+
+// Executar uma acao
+function cloneField() {
+    // Duplicar os campos. Que campos?
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true) //bolean: true ou false
+    
+    // pegar os campos. Que campos?
+    const fields = newFieldContainer.querySelectorAll('input')
+
+    // para cada campo, limpar
+    fields.forEach(function(field) {
+        field.value = ""
+    })
+
+    // Colocar na página: onde??
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
